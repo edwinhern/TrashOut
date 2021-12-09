442fa154d5dd3f30fb2214deeb86e3659fe004dc
Edwin Hernandez <Edwinhernandez2@my.unt.edu>
2021-12-08 22:48:24 -0600

    updated database

--- database/data.py
+++ database/data.py
@@ -1,12 +1,17 @@
 from connect import Connect
 from pymongo import MongoClient
-import dns
 
 try:
-    client = Connect.get_connection() 
-    db = client["test"]
-    coll = db["test"]
-    coll.insert_one({"name": "Edwin"})
+    client = Connect.get_connection() # connnect to database server
+    db = client.test.test # access database.collection
+
+    mylist = [ # List of documents to add to the collection
+    { "_id": 1, "name": "Edwin", "phoneNumber": "123456789"},
+    { "_id": 2, "name": "Alex", "phoneNumber": "123456789"},
+    { "_id": 3, "name": "Treshion", "phoneNumber": "123456789"},
+    { "_id": 4, "name": "Devin", "phoneNumber": "123456789"},
+    ]
+    x = db.insert_many(mylist) # add documents to database
 except:
     print("No connection")
 finally:
